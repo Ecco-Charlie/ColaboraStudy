e954acf9902a9431ee7a34372b70650f8ebd0e63
Rabano <rabano@soft-exe.org>
2026-01-09 17:19:52 -0600

    Fix Android SplashScreen

--- composeApp/src/androidMain/kotlin/soft/exe/colabora/study/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/soft/exe/colabora/study/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.dialogs.init
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
